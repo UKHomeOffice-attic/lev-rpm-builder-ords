0511b6e47739205895c22f1098970a96c7caedf6
Billie Alice Thompson <billie@purplebooth.co.uk>
2015-06-30 14:28:50 +0100

    Added env to customise output dir

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ docker build -t rpm-builder-ords . && docker run -v $(pwd):/rpmbuild rpm-builder
 ```
 
 RPMs will fall out named ```ords-3.0.0.121.10.23-1.x86_64.rpm```
+
+You can customise the RPM output directory in the container by setting the ```$RPM_OUTPUT_DIR``` environment variable.
--- run.sh
+++ run.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
 
-mkdir -p /root/ords/opt/ords.3.0.0 /root/ords/etc/init.d/
+mkdir -p /root/ords/opt/ords.3.0.0 /root/ords/etc/init.d/ || exit 1
 # Prepare
-wget "https://www.dropbox.com/s/an70k0q7v9ku0tq/ords.3.0.0.121.10.23.zip&dl=1" -O /root/rpmbuild/ords.3.0.0.121.10.23.zip
-unzip /root/rpmbuild/ords.3.0.0.121.10.23.zip -d /root/ords/opt/ords.3.0.0
+(wget "https://www.dropbox.com/s/an70k0q7v9ku0tq/ords.3.0.0.121.10.23.zip&dl=1" \
+    -O /root/rpmbuild/ords.3.0.0.121.10.23.zip && \
+unzip /root/rpmbuild/ords.3.0.0.121.10.23.zip -d /root/ords/opt/ords.3.0.0) || exit 1
 
-cp /root/rpmbuild/config/systemv.sh /root/ords/etc/init.d/ords
-touch /root/ords/opt/ords.3.0.0/config_updated_at
-cp /root/rpmbuild/config/ords_params.properties /root/ords/opt/ords.3.0.0/params/ords_params.properties
+cp /root/rpmbuild/config/systemv.sh /root/ords/etc/init.d/ords || exit 1
+touch /root/ords/opt/ords.3.0.0/config_updated_at || exit 1
+cp /root/rpmbuild/config/ords_params.properties /root/ords/opt/ords.3.0.0/params/ords_params.properties || exit 1
 
 
-cd /root/rpmbuild/
+cd /root/rpmbuild/ || exit 1
 
-mkdir -p /root/ords/var/logs/ords/ /root/ords/var/run/ords/
+mkdir -p /root/ords/var/logs/ords/ /root/ords/var/run/ords/ || exit 1
 
 fpm -s dir -t rpm -C /root/ords/ \
     -n ords \
@@ -25,6 +26,7 @@ fpm -s dir -t rpm -C /root/ords/ \
     --after-remove postremove.sh \
     --config-files etc/init.d/ords \
     --config-files opt/ords.3.0.0/config_updated_at \
-    --config-files opt/ords.3.0.0/params/ords_params.properties
+    --config-files opt/ords.3.0.0/params/ords_params.properties || exit 1
 
-cp *.rpm /rpmbuild
+cp *.rpm "${RPM_OUTPUT_DIR:-/rpmbuild}" || exit 1
+
