7c9b2c59324ea0feea266505954ef079902254ef
Billie Alice Thompson <billie@purplebooth.co.uk>
2015-07-16 10:27:48 +0100

    Added ORDs zip to builder as dropbox has changed share

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,9 @@ RUN gem install fpm
 COPY run.sh /run.sh
 RUN chmod a+x /run.sh
 COPY rpmbuild /root/rpmbuild
+COPY ords.3.0.0.121.10.23.zip /root/rpmbuild/ords.3.0.0.121.10.23.zip
 RUN mkdir /rpmbuild
 
 WORKDIR /root
 
 ENTRYPOINT /run.sh
-
--- run.sh
+++ run.sh
@@ -3,9 +3,7 @@
 
 mkdir -p /root/ords/opt/ords.3.0.0 /root/ords/etc/init.d/ || exit 1
 # Prepare
-(wget "https://www.dropbox.com/s/an70k0q7v9ku0tq/ords.3.0.0.121.10.23.zip&dl=1" \
-    -O /root/rpmbuild/ords.3.0.0.121.10.23.zip && \
-unzip /root/rpmbuild/ords.3.0.0.121.10.23.zip -d /root/ords/opt/ords.3.0.0) || exit 1
+(unzip /root/rpmbuild/ords.3.0.0.121.10.23.zip -d /root/ords/opt/ords.3.0.0) || exit 1
 
 cp /root/rpmbuild/config/systemv.sh /root/ords/etc/init.d/ords || exit 1
 touch /root/ords/opt/ords.3.0.0/config_updated_at || exit 1
@@ -31,4 +29,3 @@ fpm -s dir -t rpm -C /root/ords/ \
 echo ${RPM_OUTPUT_DIR:-/rpmbuild}
 ls ${RPM_OUTPUT_DIR:-/rpmbuild}
 cp *.rpm "${RPM_OUTPUT_DIR:-/rpmbuild}" || exit 1
-
